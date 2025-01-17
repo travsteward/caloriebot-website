fa63fe8d3e8182b5f10d9d81c2076a61e6c640b4
travsteward <travis.steward@gmail.com>
2025-01-17 13:12:04 -0800

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        discord: '#5865F2',
+        primary: '#7289DA',
+        secondary: '#2C2F33',
+        dark: '#23272A'
+      }
+    }
+  },
+  plugins: []
+}
