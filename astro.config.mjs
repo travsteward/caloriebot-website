feefddbf36234b947db6fd36fd25ec5c8a404948
Travis Steward <travis.steward@gmail.com>
2025-01-31 18:48:49 -0800

    Upgrade Astro and dependencies, add MDX and typography support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions'; // Keep this line
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: netlify(), // Keep this line
+  integrations: [
+    tailwind(),
+    mdx()
+  ],
+  adapter: netlify({
+    imageCDN: true
+  }),
   site: 'https://caloriebot.ai',
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true
+    }
+  }
 });
